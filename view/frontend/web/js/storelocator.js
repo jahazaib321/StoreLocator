6438632aa7c4e6127391a8b55353e385a255835c
tahaMage4 <taha@mage4.com>
2022-10-10 22:39:10 +0500

    Adding css || Map Functionality

--- view/frontend/web/js/storelocator.js
+++ view/frontend/web/js/storelocator.js
@@ -1,21 +1,20 @@
 define(["uiComponent", "jquery"], function (Component, $) {
     "use strict";
     return Component.extend({
-        map: null, initialize: function (config, node ,stores, google_api_key) {
+        map: null, initialize: function (config, node, google_api_key) {
 
-            var taha = config.stores;
-            console.log("taha" , taha)
-            // sb se phele ye chele ga
+            const Stores = config.stores;
             console.log("config", config);
+            console.log("stores", Stores);
             this.renderGetCurrentLocation();
-            this.flyToLocation();
-            this.renderGoogleMap();
+            this.flyToLocation(Stores);
+            this.renderGoogleMap(Stores);
             this.SearchInputField();
         },
 
 
         renderGetCurrentLocation: function () {
-            $('.get_location').on('click', function () {
+            $('#get_location').on('click', function () {
                 if (navigator.geolocation) {
                     navigator.geolocation.getCurrentPosition(function (position) {
                         if (window.map) {
@@ -25,106 +24,110 @@ define(["uiComponent", "jquery"], function (Component, $) {
                     });
                 }
             })
-
         },
-        flyToLocation: function () {
-            $('.flyTo').on('click', function (config) {
-                setTimeout(() => {
-                    // let maps = new google.maps;
-                    let myLatlng = new google.maps.LatLng(-34.397, 150.644);
-                    map.panTo(myLatlng);
-                    // window.map.panTo(new google.maps.LatLng(24.860735, 67.001137), location);
+
+        flyToLocation: function (store) {
+            $('#flyTo').on('click', function () {
+                console.log(store, 'testing...');
+                 setTimeout(() => {
+                 let map = new google.maps.Map(document.getElementById("Google_Map"));
+                    map.panTo(new google.maps.LatLng(store.lat, store.lng));
                     map.setZoom(12);
-                    return null;
                 }, 1000);
             })
-
         },
-        renderGoogleMap: function () {
+        renderGoogleMap: function (Stores) {
+            console.log("Data Stores", Stores);
+
             let MapContent = document.getElementById("Google_Map")
             let map = new google.maps.Map(MapContent, {
-                center: {lat: 59.9695413, lng: 30.382844}, zoom: 12
+                center: {lat: 59.9695413, lng: 30.382844},
+                zoom: 12
             });
 
-            let marker = new google.maps.Marker({
-                // position: new google.maps.LatLng(24.860735, 67.001137),
-                position: new google.maps.LatLng(taha.lat, taha.lng),
-                map: map,
-                animate: google.maps.Animation.BOUNCE,
-            });
-            console.log(taha)
-            // let address = `  <div class="store-tooltip__content">
-            //              <img class="store__image" src="" />
-            //            <div class="store-tooltip__title">store name</div>
-            //
-            //             <div class="store-tooltip__buttons">
-            //                             <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${stores.lat}+${stores.lng}" target="_blank">
-            //                              <button class="store-tooltip__button__div">
-            //                              <span>
-            //                              <img class="store-tooltip__button__img" src="pinBigImageSelected">
-            //                              </span>
-            //
-            //                              </button>
-            //                              <span class="store-tooltip__button__text" > Direction </span>
-            //                              </a>
-            //                     <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${stores.lat}+${stores.lng}" target="_blank">
-            //                              <button class="store-tooltip__button__div">
-            //                              <span class="store-tooltip__button__title">
-            //                              <img class="store-tooltip__button__img" src="pinBigImageSelected" >
-            //                              </span>
-            //                              </button>
-            //                              <span class="store-tooltip__button__text"> Save </span>
-            //                              </a>
-            //                             <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${stores.lat}+${stores.lng}" target="_blank">
-            //
-            //                                <button class="store-tooltip__button__div">
-            //                              <span class="store-tooltip__button__title">
-            //                              <img class="store-tooltip__button__img" src="pinBigImageSelected" >
-            //                              </span>
-            //                              </button>
-            //                              <span class="store-tooltip__button__text"> Nearby </span>
-            //                              </a>
-            //                              </div>
-            //
-            //                                 <div class="store-tooltip__details">
-            //
-            //                              <div class="store-tooltip__address">
-            //                              <div><img class="store-tooltip__button__img" src={pinBigImageSelected}></div>
-            //                              <div>
-            //                              <P>${stores.address}</p>
-            //                              </div>
-            //                              </div>
-            //                              <div class="store-tooltip__address">
-            //                              <div>
-            //                              <img class="store-tooltip__button__img" src={pinBigImageSelected}>
-            //                              </div>
-            //                              <div>
-            //                              <P>${stores.phone}</p>
-            //                              </div>
-            //                              </div>
-            //                              </div>
-            //
-            //                             <div class="select">
-            //                            <select name="slct" id="slct">
-            //                              <option>Open Hours</option>
-            //                              <option value="1">24</option>
-            //                              <option value="2">24</option>
-            //                              <option value="3">24</option>
-            //                              <option value="4">24</option>
-            //                              <option value="5">24</option>
-            //                            </select>
-            //                        </div>
-            //                              </div>
-            //                             </div>`
-            marker.addListener('click', () => {
-
-                let infoWindow = new google.maps.InfoWindow({
-                    content: `<h1>Pop Data </h1>`
+            let marker;
+            Stores.map((store) => {
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(store.lat, store.lng),
+                    map: map,
+                    animate: google.maps.Animation.BOUNCE,
                 });
-                infoWindow.open(map, marker);
-            })
+                marker.addListener('click', () => {
+                    let infoWindow = new google.maps.InfoWindow({
+                        content: `<div class="store-tooltip__content">
+                         <img class="store__image" src={store.image} />
+                       <div class="store-tooltip__title">store name</div>
 
+                        <div class="store-tooltip__buttons">
+                                        <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
+                                         <button class="store-tooltip__button__div">
+                                         <span>
+                                         <img class="store-tooltip__button__img" src="pinBigImageSelected">
+                                         </span>
+
+                                         </button>
+                                         <span class="store-tooltip__button__text" > Direction </span>
+                                         </a>
+                                <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
+                                         <button class="store-tooltip__button__div">
+                                         <span class="store-tooltip__button__title">
+                                         <img class="store-tooltip__button__img" src="pinBigImageSelected" >
+                                         </span>
+                                         </button>
+                                         <span class="store-tooltip__button__text"> Save </span>
+                                         </a>
+                                        <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
+
+                                           <button class="store-tooltip__button__div">
+                                         <span class="store-tooltip__button__title">
+                                         <img class="store-tooltip__button__img" src="pinBigImageSelected" >
+                                         </span>
+                                         </button>
+                                         <span class="store-tooltip__button__text"> Nearby </span>
+                                         </a>
+                                         </div>
+
+                                            <div class="store-tooltip__details">
+
+                                         <div class="store-tooltip__address">
+                                         <div>
+                                         <img class="store-tooltip__button__img" src={store.image}></div>
+                                         <div>
+                                         <P>${store.address}</p>
+                                         </div>
+                                         </div>
+                                         <div class="store-tooltip__address">
+                                         <div>
+
+                                         <img class="store-tooltip__button__img" src={store.image}>
+                                         </div>
+                                         <div>
+                                         <P>${store.phone}</p>
+                                         </div>
+                                         </div>
+                                         </div>
+
+                                        <div class="select">
+                                       <select name="slct" id="slct">
+                                         <option>Open Hours</option>
+                                         <option value="1">24</option>
+                                         <option value="2">24</option>
+                                         <option value="3">24</option>
+                                         <option value="4">24</option>
+                                         <option value="5">24</option>
+                                       </select>
+                                   </div>
+                                         </div>
+                                        </div>`
+                    });
+                    infoWindow.open(map, marker);
+                })
+            });
+
+            window.map = this.renderGoogleMap;
         },
+
+
         SearchInputField: function () {
             $("#myInput").on("keyup", function () {
                 let value = $(this).val().toLowerCase();
@@ -138,4 +141,4 @@ define(["uiComponent", "jquery"], function (Component, $) {
 
     })
 });
-// googlemaps
+// google_maps
