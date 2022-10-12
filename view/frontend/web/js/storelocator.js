fab65c2b9aa286b6df3ad6faa4d0aa4721593a28
tahaMage4 <taha@mage4.com>
2022-10-12 21:52:47 +0500

    Adding changes

--- view/frontend/web/js/storelocator.js
+++ view/frontend/web/js/storelocator.js
@@ -1,127 +1,89 @@
 define(["uiComponent", "jquery"], function (Component, $) {
     "use strict";
     return Component.extend({
-        map: null, initialize: function (config, node, google_api_key) {
+        map: null, initialize: function (config, node) {
 
             const Stores = config.stores;
+            const MarkerIcon = config.marker_icon;
+            const SelectedMarkerIcon = config.selected_marker_icon;
+            const Default_Zoom = config.Default_Zoom;
+            const Map_Style = config.Map_Style;
             console.log("config", config);
-            console.log("stores", Stores);
             this.renderGetCurrentLocation();
-            this.flyToLocation();
-            this.renderGoogleMap(Stores);
+            this.renderGoogleMap(Stores, MarkerIcon, SelectedMarkerIcon, Default_Zoom, Map_Style);
             this.SearchInputField();
+            this.flyToLocation();
         },
 
 
         renderGetCurrentLocation: function () {
-            $('#get_location').on('click', function () {
+            let map = this.map;
+            $('.get_location').on('click', function () {
                 if (navigator.geolocation) {
                     navigator.geolocation.getCurrentPosition(function (position) {
-                        if (window.map) {
-                            window.map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-                        }
+                        const lat = position.coords.latitude;
+                        const lng = position.coords.longitude;
+                        console.log(`Current Latitude is ${lat} and your lolatlngngitude is ${lng}`);
+                        let center = new google.maps.LatLng(lat, lng);
+                        map.setCenter(center);
                     });
+                } else {
+                    alert("Browser doesn't support Geolocation");
                 }
+
+
             })
         },
 
         flyToLocation: function () {
+            let map = this.map;
             $('.flyTo').on('click', function () {
-                let lat = $(this).data('lat');
-                let long = $(this).data('long');
-                console.log(lat, ' -- ', long);
-                setTimeout(() => {
-                 let map = new google.maps.Map(document.getElementById("Google_Map"));
-                    map.panTo(new google.maps.LatLng(lat, long));
-                    map.setZoom(12);
-                }, 1000);
+                let lat = parseFloat($(this).data('lat'));
+                let long = parseFloat($(this).data('long'));
+                let center = new google.maps.LatLng(lat, long);
+                map.setCenter(center);
+                map.setZoom(12);
             })
         },
-        renderGoogleMap: function (Stores) {
-            let MapContent = document.getElementById("Google_Map")
-            let map = new google.maps.Map(MapContent, {
-                center: {lat: 59.9695413, lng: 30.382844},
-                zoom: 12
+        renderGoogleMap: function (Stores, MarkerIcon, SelectedMarkerIcon, Default_Zoom, Map_Style) {
+            let MapId = document.getElementById("Google_Map")
+            let center = {lat: 38.000275, lng: 23.733576}
+            this.map = new google.maps.Map(MapId, {
+                center: center,
+                zoom: Default_Zoom,
+                greatPlaceCoords: {lat: 59.9695413, lng: 30.382844},
+                clickable: true,
+                styles: Map_Style,
+                scrollwheel: true,
+                panControl: true
             });
-
-            let marker;
             Stores.map((store) => {
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(store.lat, store.lng),
-                    map: map,
-                    animate: google.maps.Animation.BOUNCE,
+                let storeLatLong = {lat: parseFloat(store.lat), lng: parseFloat(store.lng)};
+                let marker = new google.maps.Marker({
+                    position: storeLatLong, map: this.map,
+                    icon: MarkerIcon ? MarkerIcon : SelectedMarkerIcon
                 });
                 marker.addListener('click', () => {
                     let infoWindow = new google.maps.InfoWindow({
-                        content: `<div class="store-tooltip__content">
-                         <img class="store__image" src={store.image} />
-                       <div class="store-tooltip__title">store name</div>
-
-                        <div class="store-tooltip__buttons">
-                                        <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
-                                         <button class="store-tooltip__button__div">
-                                         <span>
-                                         <img class="store-tooltip__button__img" src="pinBigImageSelected">
-                                         </span>
-
-                                         </button>
-                                         <span class="store-tooltip__button__text" > Direction </span>
-                                         </a>
-                                <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
-                                         <button class="store-tooltip__button__div">
-                                         <span class="store-tooltip__button__title">
-                                         <img class="store-tooltip__button__img" src="pinBigImageSelected" >
-                                         </span>
-                                         </button>
-                                         <span class="store-tooltip__button__text"> Save </span>
-                                         </a>
-                                        <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
-
-                                           <button class="store-tooltip__button__div">
-                                         <span class="store-tooltip__button__title">
-                                         <img class="store-tooltip__button__img" src="pinBigImageSelected" >
-                                         </span>
-                                         </button>
-                                         <span class="store-tooltip__button__text"> Nearby </span>
-                                         </a>
-                                         </div>
-
-                                            <div class="store-tooltip__details">
-
-                                         <div class="store-tooltip__address">
-                                         <div>
-                                         <img class="store-tooltip__button__img" src={store.image}></div>
-                                         <div>
-                                         <P>${store.address}</p>
-                                         </div>
-                                         </div>
-                                         <div class="store-tooltip__address">
-                                         <div>
-
-                                         <img class="store-tooltip__button__img" src={store.image}>
-                                         </div>
-                                         <div>
-                                         <P>${store.phone}</p>
-                                         </div>
-                                         </div>
-                                         </div>
-
-                                        <div class="select">
-                                       <select name="slct" id="slct">
-                                         <option>Open Hours</option>
-                                         <option value="1">24</option>
-                                         <option value="2">24</option>
-                                         <option value="3">24</option>
-                                         <option value="4">24</option>
-                                         <option value="5">24</option>
-                                       </select>
-                                   </div>
-                                         </div>
-                                        </div>`
+                        content: `
+                         <div class="store-tooltip__content">
+                         <div>${store.image}</div>
+                         <div class="store-tooltip__title">
+                         <h2>${store.name}</h2>
+                         </div>
+                         <div class="store-tooltip__buttons">
+                           <a href="http://maps.google.com/maps?z=12&t=m&q=loc:${store.lat}+${store.lng}" target="_blank">
+                           <button class="store-tooltip__button__div">
+                            <span>direction</span>
+                            </button>
+                             </a>
+                              </div>
+                               </div>`
                     });
-                    infoWindow.open(map, marker);
+                    infoWindow.open(this.map, marker);
                 })
             });
+
 
             window.map = this.renderGoogleMap;
         },
@@ -130,7 +92,7 @@ define(["uiComponent", "jquery"], function (Component, $) {
         SearchInputField: function () {
             $("#myInput").on("keyup", function () {
                 let value = $(this).val().toLowerCase();
-                $("#myDiv *").filter(function () {
+                $("#myDiv li").filter(function () {
                     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
                 });
             });
